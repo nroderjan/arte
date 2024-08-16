2024fc5c1efb9b5b375a5a3990deace2b70474c8
nroderjan <nicoly.roderjan@gmail.com>
2024-08-16 10:46:54 -0300

    adicionando os arquivos

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+  palavra = palavraAleatoria();
+  
+}
+
+function palavraAleatoria(){
+  let palavras = ["alan xuxu", "nicoly", "amor eterno"];
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("rgb(149,148,184)");
+  fill(" rgb(6,6,12")
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo){
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+function draw() {
+ inicializaCores();
+  
+  
+  let texto = palavraParcial (0, width);
+  text(texto, 200, 200);
+}
+
+ function lugaresBacanasParaConhecer() {
+   
+ }
